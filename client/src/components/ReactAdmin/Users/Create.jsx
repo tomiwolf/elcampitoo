79a62b8835ba9443408804a787318140d4761319
tomiwolf <tomiwolfman@gmail.com>
2022-12-16 17:04:48 -0300

    master

--- client/src/components/ReactAdmin/Users/Create.jsx
+++ client/src/components/ReactAdmin/Users/Create.jsx
@@ -1,14 +1,37 @@
-import { Create, SimpleForm, TextInput } from "react-admin";
+import { Create, SimpleForm, TextInput, ImageInput, SelectInput,
+   ImageField } from "react-admin";
+
+const convertStringToNumber = value => {
+    const float = parseFloat(value);
+    return isNaN(float) ? null : float;
+};
+      
 
 const PostCreate = (props) => {
   return (
     <Create title={"Crear usuario"} {...props}>
       <SimpleForm>
-        <TextInput source="data.name" label="Nombre" fullWidth />
-        <TextInput source="data.email" label="Email" fullWidth />
-        <TextInput source="data.age" label="Edad" fullWidth />
-        <TextInput source="data.roles" label="Rol" fullWidth />
-        <TextInput source="data.pass" label="Contraseña" fullWidth />
+        <TextInput source="name" label="Nombre" fullWidth />
+        <TextInput source="email" label="Email" fullWidth />
+        <TextInput source="age" label="Edad" type="number" parse={convertStringToNumber} fullWidth />
+        <TextInput source="pass" label="Contraseña" fullWidth />
+        <SelectInput source="roles" labelpublic="Rol" choices={[
+            { id: 'public', name: 'public' },
+            { id: 'superAdmin', name: 'superAdmin' },
+            { id: 'admin', name: 'admin' },
+            { id: 'voluntario', name: 'voluntario' },
+            { id: 'equipo1', name: 'equipo1' },
+            { id: 'equipo2', name: 'equipo2' },
+            { id: 'equipo3', name: 'equipo3' },
+            { id: 'visitante', name: 'visitante' },
+            { id: 'donante', name: 'donante' },
+            { id: 'padrino', name: 'padrino' },
+            { id: 'sponsor', name: 'sponsor' },
+            { id: 'adoptante', name: 'adoptante' },
+        ]} />
+        <ImageInput source="image" label="Fótografía" >
+            <ImageField source="src" title="title" />
+        </ImageInput>
       </SimpleForm>
     </Create>
   );
